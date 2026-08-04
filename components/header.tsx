8e691c080469e6f289616a9a3c6aaf3b39835569
gutsznm <denisahendra123@gmail.com>
2026-08-04 20:56:11 +0700

    fix: bug theme class

--- components/header.tsx
+++ components/header.tsx
@@ -4,7 +4,7 @@ import { ToggleTheme } from "@/components/ui/toggle-theme";
 
 export function Header() {
   return (
-    <header className="w-full py-6">
+    <header className="w-full py-4">
       <div className="container mx-auto px-4">
         <div className="flex items-center justify-end">
           <ToggleTheme />
--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,17 +1,9 @@
+// components/theme-provider.tsx
 "use client";
 
-import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 
-type ThemeProviderProps = {
-  children: React.ReactNode;
-  attribute?: string;
-  defaultTheme?: string;
-  enableSystem?: boolean;
-  disableTransitionOnChange?: boolean;
-};
-
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+export function ThemeProvider({ children, ...props }: any) {
   return (
     <NextThemesProvider
       attribute="class"
